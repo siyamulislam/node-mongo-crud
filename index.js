752890c96859965a32b8513a9b56bd9e4e44cee3
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-09 00:04:45 +0600

    mongoDB connected

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const port = 3000
+ const userName="SiyamIDT"
+ const password= '4Juc@nnXH7EKjm9'
+ const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://SiyamIDT:nnXH7EKjm9@cluster0.7oely.mongodb.net/organicDB?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app=express();
+
+
+client.connect(err => {
+  const collection = client.db("organicDB").collection("products");
+  // perform actions on the collection object
+  console.log('connected');
+  client.close();
+});
+
 app.get('/',(req,res)=>{
     res.send('hello I am work')
 })
 app.listen(port);
+
