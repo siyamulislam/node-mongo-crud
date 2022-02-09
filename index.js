cb062f6757b7505e3f1ba68407bbfa2e02a3237b
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-09 17:57:44 +0600

    mongoDB data insertone

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 const port = 3000
- const userName="SiyamIDT"
- const password= '4Juc@nnXH7EKjm9'
- const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://SiyamIDT:nnXH7EKjm9@cluster0.7oely.mongodb.net/organicDB?retryWrites=true&w=majority";
+const userName = "SiyamIDT"
+const password = 's6HoINYtsmTDvhca'
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://SiyamIDT:s6HoINYtsmTDvhca@cluster0.7oely.mongodb.net/organicDB?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const app=express();
+
+const app = express();
 
 
 client.connect(err => {
-  const collection = client.db("organicDB").collection("products");
-  // perform actions on the collection object
-  console.log('connected');
-  client.close();
-});
+    const collection = client.db("organicDB").collection("products");
+    // perform actions on the collection object
+       //  addProduct(collection)
 
-app.get('/',(req,res)=>{
+    
+   const product= {name:'modhu',quantity:87,price:980}
+   collection.insertOne( product )
+   .then(result=> console.log("inserted"))
+    
+    console.log('db connected');
+      client.close()
+});
+async function addProduct(collection){
+    const doc = { name: "Neapolitan pizza", shape: "round" };
+    const result = await collection.insertOne(doc)
+    .then(result=>{
+        console.log(
+            `A document was inserted with the _id: ${result.insertedId}`,
+         ); 
+    })
+        
+}
+app.get('/', (req, res) => {
     res.send('hello I am work')
 })
 app.listen(port);
